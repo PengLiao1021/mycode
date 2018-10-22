c7ff0ef08ee84f11532d30bcf709239555d57267
Peng <peng@fake.com>
2018-10-22 17:45:22 -0400

    input change

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -3,3 +3,4 @@ user_input = input('Please enter an IPv4 IP address:')
 print("You told me the IPv4 address is:" + user_input)
 user_input2 = input('enter the vendor name for SPC:')
 print("vendor name for SPC is:" + user_input2)
+add
